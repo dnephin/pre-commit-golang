94241f30ef6036da8234e22ec2ada1e33aa9f5b7
黄璞 <awsomekde@gmail.com>
2020-11-12 16:02:07 +0800

    support pre-commit exclude

--- run-go-critic.sh
+++ run-go-critic.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
-exec gocritic check ./...
+
+failed=false
+
+for file in "$@"; do
+    # redirect stderr so that violations and summaries are properly interleaved.
+    if ! gocritic check "$file" 2>&1
+    then
+        failed=true
+    fi
+done
+
+if [[ $failed == "true" ]]; then
+    exit 1
+fi
