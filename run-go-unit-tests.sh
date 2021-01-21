5d8a197c13af32a5e8c3bf14a896c290a200d35f
ariefaprilianto <arief.aprilianto@tokopedia.com>
2021-01-22 04:37:05 +0700

    Adjust go test

--- run-go-unit-tests.sh
+++ run-go-unit-tests.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-FILES=$(go list ./...  | grep -v /vendor/)
-
-go test -tags=unit -timeout 30s -short -v ${FILES}
+go test -v -coverprofile=c.out `go list ./...`
 
 returncode=$?
 if [ $returncode -ne 0 ]; then
