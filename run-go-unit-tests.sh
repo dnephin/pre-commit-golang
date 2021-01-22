eebd1cb2d30a329de77bb0e7f6bae44cedcde352
ariefaprilianto <arief.aprilianto@tokopedia.com>
2021-01-22 10:48:48 +0700

    Fix config

--- run-go-unit-tests.sh
+++ run-go-unit-tests.sh
@@ -2,6 +2,7 @@
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 remotebanch = "origin/$branch"
 
+echo "$remotebanch"
 FILES=$(git diff --stat --cached $remotebanch)
 
 # loop againts files
